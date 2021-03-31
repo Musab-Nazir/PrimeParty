f8e41ff1e54f239b9adaee5b040c4747dc5060bd
Musab Nazir <musabnazir@gmail.com>
2021-03-30 23:21:05 -0400

    Added files

---
+++ core.clj
@@ -0,0 +1,59 @@
+(ns test.core
+  (:require [clojure.core.reducers :as r]))
+
+(defn sieve
+  [^long n]
+  (loop [primes (range 2 4)
+         candidates (range 5 (inc n) 2)
+         p 3]
+    (if
+     (> (Math/pow p 2) n)
+      (concat primes candidates)
+      (let [remaining (remove #(= 0 (rem % p)) candidates)
+            next-p (take 1 remaining)]
+        (recur
+         (concat primes next-p)
+         (drop 1 remaining)
+         (first next-p))))))
+
+(last (sieve 1000))
+(time (last (sieve 1000000))) ; .48s approx
+
+;; taken from: https://clojureverse.org/t/eratosthenes-party-time-a-k-a-feedback-wanted-on-this-implementation-of-eratosthenes-sieve/3801
+(defn primes-below [^long n]
+  (let [primes (boolean-array (inc n) true)
+        sqrt-n (int (Math/ceil (Math/sqrt n)))]
+    (if (< n 2)
+      '()
+      (loop [p 3]
+        (if (< sqrt-n p)
+          (concat '(2)
+                  (filter #(aget primes %)
+                          (range 3 (inc n) 2)))
+          (do
+            (when (aget primes p)
+              (loop [i (* p p)]
+                (if (<= i n)
+                  (do
+                    (aset primes i false)
+                    (recur (+ i p p))))))
+            (recur  (+ p 2))))))))
+
+(time (last (primes-below 1000000))) ;9ms how?!
+
+;; The Sieve of Eratosthenes naive implementation
+(defn sieve-reduced
+  [n]
+  (let [numbers (conj (range 3 (inc n) 2) 2)
+        stuff (r/reduce
+               (fn
+                 [val num]
+                 (into []
+                       (r/remove #(when (not= % num)
+                                    (zero? (mod % num)))
+                                 val)))
+               numbers
+               numbers)]
+    stuff))
+
+(time (last (sieve-reduced 1000000))) ; 906s approx yikes
